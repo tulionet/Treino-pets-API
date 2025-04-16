5a5a6a026236ccca85d0eca4d6117003ed8d9422
tulio.neto <tulio.jacinto32@gmail.com>
2025-04-16 10:21:51 -0300

    Alteração artefato do projeto.

---
+++ src/main/java/org/backend_pets/safe_pets/SafePetsApplication.java
@@ -0,0 +1,13 @@
+package org.backend_pets.safe_pets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafePetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafePetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/backend_pets/safe_pets/SafePetsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.backend_pets.safe_pets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafePetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
